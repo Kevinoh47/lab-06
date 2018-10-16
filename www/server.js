91487a73e25dcbbb78c3226927b2d7df087575ff
Kevinoh47 <kohok47@gmail.com>
2018-10-16 14:40:54 -0700

    json-server db.json and basic store site routes working

---
+++ www/server.js
@@ -0,0 +1,89 @@
+'use strict';
+
+const superagent = require('superagent');
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+// database API can be found here:
+const API = process.env.API_URL || 'http://localhost:3000';
+
+// set up EJS as web server templating engine
+app.set('view engine', 'ejs');
+
+// public folder serves static assets:
+app.use(express.static('./public'));
+
+// routes
+app.get('/', homePage);
+
+app.get('/products', productsPage);
+
+app.get('/categories', categoriesPage);
+
+app.get('/category/:id', categoryProductsPage);
+
+// helper callbacks
+function homePage (req, res) {
+  res.render(
+    'site', 
+    {
+      page:'./pages/home', 
+      title:'HDGS Home Page',
+    }
+  );
+}
+
+function productsPage (req, res) {
+  superagent.get(`${API}/products`)
+    .then(data => {
+      res.render(
+        'site', 
+        {
+          products:data.body, 
+          page:'./pages/products', 
+          title:'HDGS Products',
+        }
+      );
+    });
+}
+
+function categoriesPage (req, res) {
+  superagent.get(`${API}/categories`)
+    .then(data => {
+      res.render(
+        'site', 
+        {
+          categories:data.body, 
+          page:'./pages/categories', 
+          title:'HDGS Product Categories',
+        }
+      );
+    });
+}
+/*request
+    .get('/querystring')
+    .query('search=Manny')
+    .query('range=1..5')
+    .then(res => {
+
+    }); */
+function categoryProductsPage (req, res) {
+  //console.log('id: ', req.params.id);
+  superagent.get(`${API}/products`)
+    .query(`category=${req.params.id}`)
+    .then(data => {
+      res.render(
+        'site',
+        {
+          products:data.body,
+          page:'./pages/categoryProducts',
+          title:'HDGS Products by Category',
+        }
+      );
+    });
+}
+
+// listener
+app.listen(PORT, () => {`Web server listening on PORT ${PORT}`;});
